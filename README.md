9a6326df55a8d44fe06cc3cc409aaa2a83d4aab5
heliomsolivas-solucx <helio@solucx.com.br>
2020-05-27 00:46:17 -0300

    Adding to do and some front on home

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # cade-a-loja
 
-> My amazing Nuxt.js project
+> Projeto para divulgar Lojas do comercio, com informações de delivery
+
+> Projeto feito com Nuxt com os dados guardados em um JSON, utilizando o novo módulo [@nuxtjs/content](https://content.nuxtjs.org/)
+
+> Se quiser ajudar faça seu PR com algo da lista TODO abaixo.
+
+# TODO
+
+- Estruturar imagens das lojas
+- Cadastrar mais lojas no JSON referente as lojas
+- Estruturar componentes do projeto
+- Criar cabeçalho como Busca
+- Criar busca (fuzzy search ou utilizar as querys do Nuxt)
+- Criar view de detalhes das lojas
+- Criar footer
+- Criar guia de contribuição
+- Pensar em ideias no nome do Webapp
+- Instalar módulo do Google Analytics
 
 ## Build Setup
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="container">
+    <h1>Lista de Lojas</h1>
     <div class="grid">
       <div class="grid__item" v-for="(store, i) in stores" :key="i">
-        {{ store.name }}
+        <div class="badge">{{ store.category }}</div>
+        <div class="name">{{ store.name }}</div>
+        <div class="details">{{ store.address }}</div>
+
+        <div class="social">
+          <img width="32" src="@/assets/icons/instagram.png" />
+          <a :href="`https://api.whatsapp.com/send?phone=${store.phone}`"
+            ><img width="32" src="@/assets/icons/whatsapp.png"
+          /></a>
+          <img width="32" src="@/assets/icons/facebook.png" />
+        </div>
       </div>
     </div>
   </div>
@@ -28,6 +39,69 @@ export default {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
+h1 {
+  margin-bottom: 32px;
+  font-size: 32px;
+}
+.delivery {
+  display: flex;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto,
+    'Helvetica Neue', sans-serif !important;
+  color: rgb(34, 34, 34) !important;
+}
+.social {
+  margin-top: 8px;
+  display: grid;
+  grid-gap: 8px;
+  grid-template-columns: 1fr 1fr 1fr;
+  max-width: 120px;
+}
+.name {
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto,
+    'Helvetica Neue', sans-serif !important;
+  font-weight: 400 !important;
+  color: rgb(34, 34, 34) !important;
+  font-size: 16px !important;
+  line-height: 20px !important;
+}
+.badge {
+  background-color: rgb(255, 255, 255) !important;
+  color: rgb(34, 34, 34) !important;
+  display: inline-block !important;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto,
+    'Helvetica Neue', sans-serif !important;
+  font-weight: 600 !important;
+  text-transform: uppercase !important;
+  text-align: center !important;
+  padding-top: 1px !important;
+  padding-bottom: 1px !important;
+  padding-left: 5px !important;
+  padding-right: 5px !important;
+  font-size: 12px !important;
+  line-height: 16px !important;
+  border-radius: 4px !important;
+  border-width: 1px !important;
+  border-style: solid !important;
+  border-color: rgb(34, 34, 34) !important;
+  border-image: initial !important;
+  margin-bottom: 8px;
+}
+.details {
+  color: rgb(113, 113, 113) !important;
+  line-height: 20px !important;
+  max-height: 20px !important;
+  text-overflow: ellipsis !important;
+  display: -webkit-box !important;
+  -webkit-line-clamp: 1 !important;
+  -webkit-box-orient: vertical !important;
+  animation-duration: 0.3s !important;
+  animation-name: keyframe_18jn58a !important;
+  animation-timing-function: ease-in-out !important;
+  opacity: 1 !important;
+  visibility: visible !important;
+  flex: 1 1 0% !important;
+  overflow: hidden !important;
+}
 .grid {
   width: 100%;
   position: relative;
@@ -42,12 +116,15 @@ export default {
   border: 1px solid #f7f7f7;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.08);
   color: #222222;
+  text-align: left;
+  padding: 16px;
 }
 .container {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
   text-align: center;
 }
